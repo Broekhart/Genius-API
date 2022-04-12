96c195867ee8c5530a442bb6206397a366a53b5b
Broekhart <niccoloruggieri.it>
2022-04-12 15:49:24 +0200

    ultime righe di codice superflue

--- src/Blocks.js
+++ src/Blocks.js
@@ -1,5 +1,3 @@
-import {useState} from 'react'
-
 // Artist e img: l'array dei nomi e immagini degli artisti trovati cliccando la barra di ricerca.
 // newSection: la funzione che al click dell'artista o immagine scelta, reinderizza alla nuova sezione di esso
 // options, border: due stringhe che servono per cambiare la classe CSS come si vuole
@@ -8,15 +6,15 @@ const Blocks = ({artist, img, newSection, options, border}) => {
 return(
   <main className={options}>
       <section className={border}>
-      <img onClick={() => {newSection(0)}} src={img[0]} />
+      <img alt="" onClick={() => {newSection(0)}} src={img[0]} />
         <p onClick={() => {newSection(0)}}> {artist[0]} </p>
       </section>
       <section className={border}>
-      <img onClick={() => {newSection(1)}} src={img[1]} />
+      <img alt="" onClick={() => {newSection(1)}} src={img[1]} />
       <p onClick={() => {newSection(1)}}> {artist[1]} </p>
        </section>
       <section className={border}>
-      <img onClick={() => {newSection(2)}} src={img[2]} />
+      <img alt="" onClick={() => {newSection(2)}} src={img[2]} />
       <p onClick={() => {newSection(2)}}> {artist[2]} </p>
       </section>
   </main>
