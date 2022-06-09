dff418208ec16bf8f6df3b07f075d15b55a13e5d
Broekhart <66094493+Broekhart@users.noreply.github.com>
2022-06-09 20:31:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,6 @@ function App() {
 
 // Background: il colore del background che verrà passato ai child components
   const [background, setBackground] = useState("#f1f2f0")
-// Funzione che ad ogni chiamata, in base al numero random uscito, darà un colore diverso alla variabile background.
-  const changeColors = () => {
-    const random = Math.floor(Math.random() * (3 - 1 + 1) + 1)
-    if(random === 1){
-      setBackground("#ffbf66")
-    }
-    if(random === 2){
-      setBackground("#C7AD24")
-    }
-  }
-  changeColors()
 
   return (
    <Router>
